88052dbd70f71e7d72fa9369a205c2a533284e77
Ahsan Gulzar <ahsangulzar009@gmail.com>
2025-11-25 19:22:12 +0500

    About me problem solved

--- src/constants/index.js
+++ src/constants/index.js
@@ -406,7 +406,7 @@ const ABOUT_LOCATION = {
          kind: "file",
          fileType: "img",
          position: "top-10 left-5",
-         imageUrl: "/images/adrian.jpg",
+         imageUrl: "/images/ahsan_2.jpeg",
       },
       {
          id: 2,
@@ -415,7 +415,7 @@ const ABOUT_LOCATION = {
          kind: "file",
          fileType: "img",
          position: "top-28 right-72",
-         imageUrl: "/images/adrian-2.jpg",
+         imageUrl: "/images/ahsan_2.jpeg",
       },
       {
          id: 3,
@@ -424,7 +424,7 @@ const ABOUT_LOCATION = {
          kind: "file",
          fileType: "img",
          position: "top-52 left-80",
-         imageUrl: "/images/adrian-3.jpeg",
+         imageUrl: "/images/ahsan_2.jpeg",
       },
       {
          id: 4,
@@ -434,9 +434,9 @@ const ABOUT_LOCATION = {
          fileType: "txt",
          position: "top-60 left-5",
          subtitle: "Meet the Developer Behind the Code",
-         image: "/images/adrian.jpg",
+         image: "/images/ahsan_2.jpeg",
          description: [
-            "Hey! I’m Adrian 👋, a web developer who enjoys building sleek, interactive websites that actually work well.",
+            "Hey! I’m Ahsan Gulzar👋, a web developer who enjoys building sleek, interactive websites that actually work well.",
             "I specialize in JavaScript, React, and Next.js—and I love making things feel smooth, fast, and just a little bit delightful.",
             "I’m big on clean UI, good UX, and writing code that doesn’t need a search party to debug.",
             "Outside of dev work, you'll find me tweaking layouts at 2AM, sipping overpriced coffee, or impulse-buying gadgets I absolutely convinced myself I needed 😅",
